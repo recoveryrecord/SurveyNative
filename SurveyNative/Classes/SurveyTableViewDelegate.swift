07b63f65d2c40c864ddebc3bcdaf4f1fe123950b
nmullaney <nora.mullaney@gmail.com>
2020-07-16 11:02:14 -0700

    Fix crash when user submits

--- SurveyNative/Classes/SurveyTableViewDelegate.swift
+++ SurveyNative/Classes/SurveyTableViewDelegate.swift
@@ -33,8 +33,8 @@ open class SurveyTableViewDelegate : NSObject, UITableViewDelegate {
          } else {
             selectedCell.setSelectionState(!selectedCell.selectionState())
          }
+         TableUIUpdater.updateTable(self.surveyQuestions.selectedRowAt(indexPath, tableView: tableView), tableView: tableView, autoFocus: surveyQuestions.autoFocusText)
       }
-      TableUIUpdater.updateTable(self.surveyQuestions.selectedRowAt(indexPath, tableView: tableView), tableView: tableView, autoFocus: surveyQuestions.autoFocusText)
       tableView.deselectRow(at: indexPath, animated: false)
    }
    
