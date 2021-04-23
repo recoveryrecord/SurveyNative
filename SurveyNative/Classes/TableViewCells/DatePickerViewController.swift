663ddb730f614ef8f4f8198358a12b969dd0cdb0
nmullaney <nora.mullaney@gmail.com>
2021-04-23 13:34:31 -0700

    Fix dark mode for Year Picker and Date Picker

--- SurveyNative/Classes/TableViewCells/DatePickerViewController.swift
+++ SurveyNative/Classes/TableViewCells/DatePickerViewController.swift
@@ -9,44 +9,66 @@
 import UIKit
 
 class DatePickerViewController: UIViewController {
-   @IBOutlet var datePickerView: UIDatePicker?
-   
-   var controllerDelegate: DatePickerViewControllerDelegate?
-   var currentDate: Date? {
-      didSet {
-         datePickerView?.date = currentDate!
-      }
-   }
-   var minDate: Date? {
-      didSet {
-         datePickerView?.minimumDate = minDate
-      }
-   }
-   var maxDate: Date? {
-      didSet {
-         datePickerView?.maximumDate = maxDate
-      }
-   }
-   
-   override func viewDidLoad() {
-      super.viewDidLoad()
-      
-      datePickerView?.date = currentDate ?? Date()
-      datePickerView?.datePickerMode = UIDatePicker.Mode.date
-      datePickerView?.minimumDate = minDate
-      datePickerView?.maximumDate = maxDate
-   }
-   
-   override func didReceiveMemoryWarning() {
-      super.didReceiveMemoryWarning()
-      // Dispose of any resources that can be recreated.
-   }
-   
-   @IBAction func doneTapped(_ sender: UIButton) {
-      controllerDelegate?.onDone(selectedDate: datePickerView?.date)
-   }
+    @IBOutlet var datePickerView: UIDatePicker?
+    
+    var controllerDelegate: DatePickerViewControllerDelegate?
+    var currentDate: Date? {
+        didSet {
+            datePickerView?.date = currentDate!
+        }
+    }
+    var minDate: Date? {
+        didSet {
+            datePickerView?.minimumDate = minDate
+        }
+    }
+    var maxDate: Date? {
+        didSet {
+            datePickerView?.maximumDate = maxDate
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        datePickerView?.date = currentDate ?? Date()
+        datePickerView?.datePickerMode = UIDatePicker.Mode.date
+        datePickerView?.minimumDate = minDate
+        datePickerView?.maximumDate = maxDate
+        
+        if #available(iOS 13.4, *) {
+            datePickerView?.preferredDatePickerStyle = .wheels
+        } else {
+            // Fallback on earlier versions
+        }
+        setColors()
+    }
+    
+    func setColors() {
+        if #available(iOS 12.0, *) {
+            if traitCollection.userInterfaceStyle == .dark {
+                datePickerView?.backgroundColor = UIColor.black
+            } else {
+                datePickerView?.backgroundColor = UIColor.white
+            }
+        }
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        setColors()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func doneTapped(_ sender: UIButton) {
+        controllerDelegate?.onDone(selectedDate: datePickerView?.date)
+    }
 }
 
 public protocol DatePickerViewControllerDelegate : NSObjectProtocol {
-   func onDone(selectedDate: Date?);
+    func onDone(selectedDate: Date?);
 }
--- SurveyNative/Classes/TableViewCells/PickerViewController.swift
+++ SurveyNative/Classes/TableViewCells/PickerViewController.swift
@@ -9,36 +9,55 @@
 import UIKit
 
 class PickerViewController: UIViewController, UIPickerViewDelegate {
-   @IBOutlet var pickerView: UIPickerView!
-   
-   var controllerDelegate: PickerViewControllerDelegate?
-   var pickerDataSource: UIPickerViewDataSource?
-   var pickerDelegate: UIPickerViewDelegate?
-   var initialSelectedRow : Int?
-   
-   override func viewDidLoad() {
-      super.viewDidLoad()
-      
-      if pickerView != nil {
-         pickerView.delegate = pickerDelegate
-         pickerView.dataSource = pickerDataSource
-         
-         if initialSelectedRow != nil {
-            pickerView.selectRow(initialSelectedRow!, inComponent: 0, animated: true)
-         }
-      }
-   }
-   
-   override func didReceiveMemoryWarning() {
-      super.didReceiveMemoryWarning()
-      // Dispose of any resources that can be recreated.
-   }
-   
-   @IBAction func doneTapped(_ sender: Any) {
-      controllerDelegate?.onDone()
-   }
+    @IBOutlet var pickerView: UIPickerView!
+    
+    var controllerDelegate: PickerViewControllerDelegate?
+    var pickerDataSource: UIPickerViewDataSource?
+    var pickerDelegate: UIPickerViewDelegate?
+    var initialSelectedRow : Int?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if pickerView != nil {
+            pickerView.delegate = pickerDelegate
+            pickerView.dataSource = pickerDataSource
+            
+            if initialSelectedRow != nil {
+                pickerView.selectRow(initialSelectedRow!, inComponent: 0, animated: true)
+            }
+            
+            setColors()
+        }
+    }
+    
+    func setColors() {
+        if #available(iOS 12.0, *) {
+            if traitCollection.userInterfaceStyle == .dark {
+                pickerView.backgroundColor = UIColor.black
+            } else {
+                pickerView.backgroundColor = UIColor.white
+            }
+        } else {
+            pickerView.tintColor = UIColor.black
+        }
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        setColors()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func doneTapped(_ sender: Any) {
+        controllerDelegate?.onDone()
+    }
 }
 
 public protocol PickerViewControllerDelegate : NSObjectProtocol {
-   func onDone();
+    func onDone();
 }
